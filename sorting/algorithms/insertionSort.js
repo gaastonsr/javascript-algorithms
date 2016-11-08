aafdc1505ac3c6ee02b4635a2ffc9e0e2422df46
Gaston Sanchez <gaastonsr@gmail.com>
2016-11-08 12:12:16 -0600

    Fix insertion sort

--- sorting/algorithms/insertionSort.js
+++ sorting/algorithms/insertionSort.js
@@ -1,9 +1,9 @@
 const { isLessThan, exchange } = require('../utils');
 
 function insertionSort(array) {
-    for (let i = 0; i < array.length; i++) {
-        for (let j = i; j > 0 && isLessThan(array[i], array[j - 1]); j++) {
-            exchange(array, i, j);
+    for (let i = 1; i < array.length; i++) {
+        for (let j = i; j > 0 && isLessThan(array[j], array[j - 1]); j--) {
+            exchange(array, j, j - 1);
         }
     }
 }
